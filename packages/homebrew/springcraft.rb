1e30d626415e12726bae608ae492dbaf2380bb32
fmitesh007 <markhackit@gmail.com>
2026-04-25 01:48:14 +0530

    chore: Update package versions to v0.3.1
    - homebrew: Update sha256 and version
    - scoop: Update sha256 and version

--- packages/homebrew/springcraft.rb
+++ packages/homebrew/springcraft.rb
@@ -3,10 +3,10 @@ require 'brewkit/formula'
 class Springcraft < Formula
   desc 'Modern CLI scaffolder for Spring Boot projects'
   homepage 'https://github.com/fmitesh007/springcraft-cli'
-  url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.0/springcraft-macos'
-  sha256 '7c6b45ba39f28aa5dd116672b427a3b919cf2376bfbf1126cad32213f5f42c17'
+  url 'https://github.com/fmitesh007/springcraft-cli/releases/download/v0.3.1/springcraft-macos'
+  sha256 '7edf7c5e3506fb9cecdc7eb92acdd25c73f7450742ed85824c2994a51fa31879'
   license 'MIT'
-  version '0.3.0'
+  version '0.3.1'
 
   def install
     bin.install 'springcraft-macos' => 'springcraft'
